957a905e30de1b36d9f00300e0937ee411208e4f
plwu1024 <plwu1024@gmail.com>
2021-06-06 03:20:10 +0800

    add some testcases.

---
+++ testcases/3_G1.v
@@ -0,0 +1,6 @@
+module top (a, b, c, o1);
+input a, b, c;
+output o1;
+wire a, b, c, o1;
+and g1 (o1, a, b, c);
+endmodule
---
+++ testcases/3_R2.v
@@ -0,0 +1,7 @@
+module top (a, b, c, o1);
+input a, b, c;
+output o1;
+wire a, b, c, o1, n1;
+or g2 (o1, a, n1);
+and g1 (n1, b, c);
+endmodule
